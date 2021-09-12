18a47a4ec80718930db784b3d98feb61c932b371
Alvaro1599 <alvarocanales1599@gmail.com>
2021-09-11 21:46:46 -0500

    branch2

---
+++ dist/app.js
@@ -0,0 +1,41 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importStar(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const auth_1 = __importDefault(require("./routes/auth"));
+require("./database");
+const app = (0, express_1.default)();
+//settings
+app.set("PORT", 4000);
+//middlwares
+app.use((0, morgan_1.default)("dev"));
+app.use((0, express_1.json)());
+app.use((0, express_1.urlencoded)({
+    extended: false,
+}));
+//routes
+app.use("/api/auth", auth_1.default);
+exports.default = app;
+//# sourceMappingURL=app.js.map
